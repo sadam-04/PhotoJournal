dbfa6c7ccbdeeaa29ac2564fe8f812cf40e670f7
Adam <156256297+sadam-04@users.noreply.github.com>
2025-02-24 19:46:42 +0100

    upload styling
    incremental

--- journal/models.py
+++ journal/models.py
@@ -6,3 +6,4 @@ class JournalEntry(models.Model):
     image = models.ImageField(upload_to='uploads/', null=True, blank=True)
     date = models.DateField()
     time = models.TimeField()
+    description = models.CharField(max_length=1024)
